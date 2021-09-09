f63a87f8a96d031bc8da35421cb4fabf433c00b5
Néstor Velázquez Estupiñán <nestorfrego@gmail.com>
2021-09-09 09:59:32 +0100

    feat(count): count displayed

--- src/components/count/index.js
+++ src/components/count/index.js
@@ -1,3 +1,32 @@
+import "./style.css";
+import useFruitCount from "../../hooks/use-fruit-count";
+
+const countStates = {
+  sad: {
+    icon: "",
+    text: "Your belly is sad!",
+  },
+  soso: {
+    icon: "",
+    text: "Your belly is so so ...",
+  },
+  happy: {
+    icon: "",
+    text: "Your belly is happy!",
+  },
+};
+
 export function Count() {
-  return <div></div>;
+  const { data } = useFruitCount();
+
+  let status;
+  if (parseInt(data) < 1) status = "sad";
+  else if (parseInt(data) > 4) status = "happy";
+  else status = "soso";
+
+  return (
+    <div className={`${status} count`}>
+      <p>{countStates[status].text}</p>
+    </div>
+  );
 }
